de87efa46b9f54f6e048bf3a6c5f3390a6245ade
MERKATOR <MERKATOR@DESKTOP-IKG681A>
2019-05-03 12:46:29 +0300

    Work commit;

--- src/AccountAndJwt.Api/Middleware/SwaggerMiddleware.cs
+++ src/AccountAndJwt.Api/Middleware/SwaggerMiddleware.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.PlatformAbstractions;
 using Swashbuckle.AspNetCore.Swagger;
 using System.IO;
+using System.Reflection;
 
 namespace AccountAndJwt.Api.Middleware
 {
@@ -10,28 +11,34 @@ namespace AccountAndJwt.Api.Middleware
 	{
 		public static void AddConfiguredSwaggerGen(this IServiceCollection services)
 		{
+			var test = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+
 			services.AddSwaggerGen(c =>
 			{
 				c.SwaggerDoc("AccountAndJwt", new Info
 				{
-					Title = "AccountAndJwt API",
-					Version = "v1"
+					Version = "v1",
+					Title = "AccountAndJwt",
+					Description = "AccountAndJwt API",
+					TermsOfService = "None",
+					Contact = new Contact()
+					{
+						Name = "Aleksandrov Evgeniy",
+						Email = "ulthane2160@gmail.com"
+					}
 				});
 				c.IncludeXmlComments(Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "Documentation.xml"));
 				c.IgnoreObsoleteActions();
 				c.IgnoreObsoleteProperties();
+				c.DescribeAllEnumsAsStrings();
 			});
 		}
 		public static void UseConfiguredSwagger(this IApplicationBuilder app)
 		{
-			app.UseSwagger(c =>
-			{
-				c.RouteTemplate = "api-docs/{documentName}/swagger.json";
-			});
+			app.UseSwagger();
 			app.UseSwaggerUI(c =>
 			{
-				c.RoutePrefix = "api-docs";
-				c.SwaggerEndpoint("/api-docs/AccountAndJwt/swagger.json", "AccountAndJwt API V1");
+				c.SwaggerEndpoint("/swagger.json", "AccountAndJwt API");
 			});
 		}
 	}
